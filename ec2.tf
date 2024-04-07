9ca5b7354cda863a4399ce8872447bc4db78b93a
Shubhonkar_Barua <127679232+shubhonkar035@users.noreply.github.com>
2024-04-07 16:27:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "demo-server" {
-  ami = "ami-09298640a92b2d12c"
-  key_name = "terraformkeypair"
+  ami = "ami-09298....."
+  key_name = "any"
   instance_type  = "t2.micro"
 }
